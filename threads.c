ecf1d0d53140c45025ed08cc67591f1fa9627395
Turlough Mullan <duke1199sp@gmail.com>
2021-07-01 18:46:45 +0200

    Off to the store for some dinner

--- threads.c
+++ threads.c
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/07/01 16:57:26 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/07/01 18:03:08 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/07/01 18:46:07 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,15 @@ void	*thread_func(void *arg)
 	t_philo	*philo = (t_philo *)arg;
 	int	left = philo->philosopher - 1;
 	int right = philo->philosopher;
-	int	i = 0;
+	int	meal_count = 0;
 
 	if (philo->philosopher == philo->table->num_philos)
 		right = 0;
 	while (1)
 	{
 		gettimeofday(&philo->table->current_time, NULL);
-		printf("|%llu| Philosopher |%d| starts thinking\n", (unsigned long long)philo->table->current_time.tv_usec - philo->table->start_time, philo->philosopher);
 		pthread_mutex_lock(&philo->table->ch_stick[left]);
 		{
-			i++;
 			philo->left = true;
 		}
 		pthread_mutex_unlock(&philo->table->ch_stick[left]);
@@ -37,6 +35,7 @@ void	*thread_func(void *arg)
 		gettimeofday(&philo->table->current_time, NULL);
 		if (philo->right && philo->left)
 		{
+			meal_count++;
 			gettimeofday(&philo->table->current_time, NULL);
 			printf("|%llu| Philosopher |%d| starts eating\n", (unsigned long long)philo->table->current_time.tv_usec - philo->table->start_time, philo->philosopher);
 			philo->state = EATING;
@@ -47,11 +46,16 @@ void	*thread_func(void *arg)
 			pthread_mutex_lock(&philo->table->ch_stick[right]);
 			philo->right = false;
 			pthread_mutex_unlock(&philo->table->ch_stick[right]);
-			philo->state = THINKING;
 			gettimeofday(&philo->table->current_time, NULL);
 			printf("|%llu| Philosopher |%d| has finished eating\n", (unsigned long long)philo->table->current_time.tv_usec - philo->table->start_time, philo->philosopher);
 		}
-		if (i == 33)
+		if (philo->state == EATING)
+		{
+			printf("|%llu| Philosopher |%d| is sleeping\n", (unsigned long long)philo->table->current_time.tv_usec - philo->table->start_time, philo->philosopher);
+			usleep(philo->table->to_sleep);
+			philo->state = SLEEPING;
+		}
+		if (meal_count == 33)
 			break;
 	}
 
