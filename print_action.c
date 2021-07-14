9091db7d9c50c35989d1a732e756b2df7e9a905b
Turlough Mullan <duke1199sp@gmail.com>
2021-07-14 19:22:05 +0200

    Ok, actually I had fucked the timestamp up

--- print_action.c
+++ print_action.c
@@ -6,31 +6,31 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/07/09 15:20:44 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/07/14 19:16:34 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/07/14 19:19:50 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	do_action(t_philo *philo, int action, struct timeval action_time)
+void	do_action(t_philo *philo, int action)
 {
 	long long	timestamp;
 	if (action == EATING)
 	{
-		timestamp = get_diff(action_time, philo);
+		timestamp = get_timestamp(philo);
 		gettimeofday(&philo->last_ate, NULL);
 		printf("|%lld| Philosopher |%d| is eating\n", timestamp, philo->philosopher);
 		precision_sleep(philo->table->to_eat, philo);
 	}
 	else if (action == SLEEPING)
 	{
-		timestamp = get_diff(action_time, philo);
+		timestamp = get_timestamp(philo);
 		printf("|%lld| Philosopher |%d| is sleeping\n", timestamp, philo->philosopher);
 		precision_sleep(philo->table->to_sleep, philo);
 	}
 	else if (action == THINKING)
 	{
-		timestamp = get_diff(action_time, philo);
+		timestamp = get_timestamp(philo);
 		printf("|%lld| Philosopher |%d| is thinking\n", timestamp, philo->philosopher);
 	}
 }
@@ -45,13 +45,13 @@ void	print_action(t_philo *philo, int action)
 	pthread_mutex_lock(philo->table->lock_action);
 	if (action == DIED)
 	{
-		timestamp = get_diff(action_time, philo);
+		timestamp = get_timestamp(philo);
 		printf("|%lld| Philosopher |%d| be ded\n", timestamp, philo->philosopher);
 		philo->table->sum1dead = true;
 	}
 	else if (!any1dead(philo->table))
 	{
-		do_action(philo, action, action_time);
+		do_action(philo, action);
 	}
 	pthread_mutex_unlock(philo->table->lock_action);
 	pthread_mutex_unlock(philo->lock_print);
