77a13aa36e23ea9e60ad6c7ac1bd174e8c249c15
Turlough stelfox Mullan <duke1199sp@gmail.com>
2021-07-29 15:43:41 +0200

    Normed the cleaner approach

--- eat_loop.c
+++ eat_loop.c
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/07/08 12:43:29 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/07/29 14:17:28 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/07/29 15:43:13 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,33 @@ void	drop_fork(pthread_mutex_t *ch_stick, t_philo *philo)
 	print_action(philo, DROP);
 }
 
+void	lefthand_righthand(t_philo *philo, int right, int left)
+{
+	if (philo->philosopher % 3)
+	{
+		grab_fork(&philo->table->ch_stick[left], philo);
+		grab_fork(&philo->table->ch_stick[right], philo);
+	}
+	else
+	{
+		grab_fork(&philo->table->ch_stick[right], philo);
+		grab_fork(&philo->table->ch_stick[left], philo);
+	}
+	print_action(philo, EATING);
+	precision_sleep(philo->table->to_eat, philo);
+	philo->state = EATING;
+	if (philo->philosopher % 3)
+	{
+		drop_fork(&philo->table->ch_stick[right], philo);
+		drop_fork(&philo->table->ch_stick[left], philo);
+	}
+	else
+	{
+		drop_fork(&philo->table->ch_stick[left], philo);
+		drop_fork(&philo->table->ch_stick[right], philo);
+	}
+}
+
 void	eat_loop(t_philo *philo)
 {
 	int	left;
@@ -34,31 +61,7 @@ void	eat_loop(t_philo *philo)
 	if (philo->philosopher == philo->table->num_philos)
 		right = 0;
 	if (philo->table->num_philos > 20)
-	{
-		if (philo->philosopher % 3)
-		{
-			grab_fork(&philo->table->ch_stick[left], philo);
-			grab_fork(&philo->table->ch_stick[right], philo);
-		}
-		else
-		{
-			grab_fork(&philo->table->ch_stick[right], philo);
-			grab_fork(&philo->table->ch_stick[left], philo);
-		}
-		print_action(philo, EATING);
-		precision_sleep(philo->table->to_eat, philo);
-		philo->state = EATING;
-		if (philo->philosopher % 3)
-		{
-			drop_fork(&philo->table->ch_stick[right], philo);
-			drop_fork(&philo->table->ch_stick[left], philo);
-		}
-		else
-		{
-			drop_fork(&philo->table->ch_stick[left], philo);
-			drop_fork(&philo->table->ch_stick[right], philo);
-		}
-	}
+		lefthand_righthand(philo, right, left);
 	else
 	{
 		grab_fork(&philo->table->ch_stick[left], philo);
